0adca3d0c20afff22f3bc2ad02a1c16157448222
rakhagans37 <muhammadrakha3704@gmail.com>
2023-08-01 19:28:26 +0700

    Mengatasi bug multiple print pada fitur sunrise dan sunset

--- script/script.js
+++ script/script.js
@@ -447,65 +447,69 @@ function printData(response) {
     }
 
     //Print Sun Condition
-    for (let i = 1; i <= 2; i++) {
+    try {
         const target = document.getElementById("sun-condition");
-        const createDivGap = document.createElement("div");
-        const createDiv = document.createElement("div");
-        const createDivTarget = document.createElement("div");
-        const createImg = document.createElement("img");
-        const createH3 = document.createElement("h3");
-        const createP = document.createElement("p");
-        const createPGap = document.createElement("p");
+        target.innerHTML = "";
 
-        createDiv.className = "circum-value";
-        createDivTarget.className = "card";
-        createP.textContent =
-            i === 1
-                ? new Date(Number(response.sys.sunrise + "000")).toLocaleString(
-                      "en-US",
-                      {
+        for (let i = 1; i <= 2; i++) {
+            const createDivGap = document.createElement("div");
+            const createDiv = document.createElement("div");
+            const createDivTarget = document.createElement("div");
+            const createImg = document.createElement("img");
+            const createH3 = document.createElement("h3");
+            const createP = document.createElement("p");
+            const createPGap = document.createElement("p");
+
+            createDiv.className = "circum-value";
+            createDivTarget.className = "card";
+            createP.textContent =
+                i === 1
+                    ? new Date(
+                          Number(response.sys.sunrise + "000")
+                      ).toLocaleString("en-US", {
                           hour: "numeric",
                           minute: "2-digit",
                           hour12: true,
-                      }
-                  )
-                : new Date(Number(response.sys.sunset + "000")).toLocaleString(
-                      "en-US",
-                      {
+                      })
+                    : new Date(
+                          Number(response.sys.sunset + "000")
+                      ).toLocaleString("en-US", {
                           hour: "numeric",
                           minute: "2-digit",
                           hour12: true,
-                      }
-                  );
-        createImg.src = i === 1 ? `image/Sunrise.png` : `image/Sunset.png`;
-        createImg.alt = i === 1 ? "image/Sunrise.png" : "image/Sunset.png";
-        createH3.textContent = i === 1 ? "Sunrise" : "Sunset";
-        console.log(
-            `${
+                      });
+            createImg.src = i === 1 ? `image/Sunrise.png` : `image/Sunset.png`;
+            createImg.alt = i === 1 ? "image/Sunrise.png" : "image/Sunset.png";
+            createH3.textContent = i === 1 ? "Sunrise" : "Sunset";
+            console.log(
+                `${
+                    new Date().getHours() -
+                    new Date(Number(response.sys.sunrise + "000")).getHours()
+                }`
+            );
+
+            const timeToSunrise =
                 new Date().getHours() -
-                new Date(Number(response.sys.sunrise + "000")).getHours()
-            }`
-        );
+                new Date(Number(response.sys.sunrise + "000")).getHours();
+            const timeToSunset =
+                new Date().getHours() -
+                new Date(Number(response.sys.sunset + "000")).getHours();
 
-        const timeToSunrise =
-            new Date().getHours() -
-            new Date(Number(response.sys.sunrise + "000")).getHours();
-        const timeToSunset =
-            new Date().getHours() -
-            new Date(Number(response.sys.sunset + "000")).getHours();
+            createPGap.textContent =
+                (i === 1 ? timeToSunrise : timeToSunset) +
+                (timeToSunrise < 0 || timeToSunset < 0 ? "h To Go" : "h Ago");
 
-        createPGap.textContent =
-            (i === 1 ? timeToSunrise : timeToSunset) +
-            (timeToSunrise < 0 || timeToSunset < 0 ? "h To Go" : "h Ago");
-
-        createDivGap.className = "interval";
-        createDivGap.appendChild(createPGap);
-        createDiv.appendChild(createH3);
-        createDiv.appendChild(createP);
-        createDivTarget.appendChild(createImg);
-        createDivTarget.appendChild(createDiv);
-        createDivTarget.appendChild(createDivGap);
-        target.appendChild(createDivTarget);
+            createDivGap.className = "interval";
+            createDivGap.appendChild(createPGap);
+            createDiv.appendChild(createH3);
+            createDiv.appendChild(createP);
+            createDivTarget.appendChild(createImg);
+            createDivTarget.appendChild(createDiv);
+            createDivTarget.appendChild(createDivGap);
+            target.appendChild(createDivTarget);
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
