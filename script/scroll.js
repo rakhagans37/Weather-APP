a2a6b37ebaf551c8ca9328a0ea3ece40014d1c16
rakhagans37 <muhammadrakha3704@gmail.com>
2023-08-01 09:45:19 +0700

    Menangani bug pada scroll behavior

--- script/scroll.js
+++ script/scroll.js
@@ -6,39 +6,33 @@ function doSomething() {
     //Do something with the scroll position
     navbar.classList.add("sticky");
     main.classList.add("sticky");
-
 }
 
-function doSomethingReverse(){
-    console.log("reverse")
+function doSomethingReverse() {
+    console.log("reverse");
     navbar.classList.remove("sticky");
-    main.classList.remove("sticky"); 
+    main.classList.remove("sticky");
 }
 
-function showList(){
-    console.log(navbar.classList == "sticky")
+function showList() {
+    console.log(navbar.classList == "sticky");
 }
-setInterval(showList,3000)
+setInterval(showList, 3000);
 
-    
-
-if(navbar.classList != "sticky"){
+if (navbar.classList != "sticky") {
     document.addEventListener("scroll", (event) => {
         event.preventDefault();
-        console.log("ini 2")
-        lastKnownScrollPosition = window.scrollY;
-        console.log(lastKnownScrollPosition)
-    
-        if(lastKnownScrollPosition > 249){
-            console.log("masuk")
+        console.log("ini 2");
+        let lastKnownScrollPosition = window.scrollY;
+        console.log(lastKnownScrollPosition);
+
+        if (lastKnownScrollPosition > 249) {
+            console.log("masuk");
             doSomething();
+            lastKnownScrollPosition = 10;
         }
-        if(lastKnownScrollPosition <= 0){
+        if (lastKnownScrollPosition <= 0) {
             doSomethingReverse();
         }
-    })
-} 
-
-
-
-
+    });
+}
